199787962ca309e0122a72d88aa85673da4911d5
BrainyYawson <acourageyawson369@gmail.com>
2024-06-13 22:35:15 +0000

    Add project files.

---
+++ OOP_Assignment/Class.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace OOP
+{
+    //The base class
+    class Animal
+    {
+        //Virtual method
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Mooooo");
+        }
+    }
+
+    //The derived class
+    class Dog : Animal
+    {
+        //Overriding the MakeSound method
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    //Derived class
+    class Cat : Animal
+    {
+        //Method overriding
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+}
---
+++ OOP_Assignment/Main.cs
@@ -0,0 +1,27 @@
+﻿using OOP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Assignment
+{
+    internal class OOP
+    {
+        static void Main(string[] args)
+        {
+            //Instance of Animal
+            Animal animal = new Animal();
+            //Instance of Dog
+            Dog dog = new Dog();
+            //Instance oof Cat
+            Cat cat = new Cat();
+
+            //Calling MakeSound method on each method
+            animal.MakeSound();   //Output Moooooo
+            dog.MakeSound();      //Output Bark
+            cat.MakeSound();      //Output Meow
+        }
+    }
+}
