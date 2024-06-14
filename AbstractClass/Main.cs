d96f334f02d1b68de4ebf479ebb26bbf15867c3d
BrainyYawson <acourageyawson369@gmail.com>
2024-06-14 16:10:28 +0000

    Implementing Abstract Classes and Methods

---
+++ AbstractClass/Main.cs
@@ -0,0 +1,26 @@
+﻿using Abstract;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    internal class Shape
+    {
+        static void Main(string[] args)
+        {
+            //Instances of Circle
+            Circle circle = new Circle(5.0);
+            //Instance of Rectangle
+            Rectangle rectangle = new Rectangle(2.0, 3.0);
+
+
+            //Area of circle
+            Console.WriteLine($"The area of the circle is: {circle.GetArea()}");
+            Console.WriteLine($"The area of the rectangle is: {rectangle.GetArea()}");
+        }
+    }
+}
---
+++ AbstractClass/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Transactions;
+namespace Abstract
+{
+    //Abstract class
+    abstract class Shape()
+    {
+        //Abstract method to be implemented by a derived class
+        public abstract double GetArea();
+    }
+
+    //Derived class Circle
+    class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius) 
+        {
+            Radius = radius;
+        }
+
+        //Abstract method GetArea implementing
+        public override double GetArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+
+        //Derived class Rectangle
+        class Rectangle : Shape
+        {
+            public double Width { get; set; }
+            public double Height { get; set; }
+
+            public Rectangle(double width, double height)
+            {
+                Width = width;
+                Height = height;
+            }
+
+            //Abstract method GetArea implementation
+            public override double GetArea()
+            {
+                return Width * Height;
+            }
+        }
+    }
+}
