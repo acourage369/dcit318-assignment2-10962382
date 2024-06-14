7229be702a5c77ea081b925f120a54686af8a269
BrainyYawson <acourageyawson369@gmail.com>
2024-06-14 16:32:44 +0000

    Interfaces

---
+++ Interfaces/Interface.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Interfaces
+{
+    //IMovable Interface
+    public interface IMovable
+    {
+        //A void method Move
+        void Move();
+
+    }
+
+    //Car class implementing IMovable Interface
+    public class Car : IMovable
+    {
+        public void Move() 
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    //Bicycle class implementing IMovable Interface
+    public class Bicycle : IMovable
+    {
+        public void Move() 
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+}
---
+++ Interfaces/Main.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    internal class Interface
+    {
+        static void Main(string[] args)
+        {
+            //Instance of Car
+            Car car = new Car();
+            //Instance of Bicycle
+            Bicycle bicycle = new Bicycle();
+
+            car.Move();
+            bicycle.Move();
+        }
+    }
+}
